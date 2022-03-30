bdf59c7b1d4df61ef1f462d0c76fc8047d4d2a6a
alex <alex@PC-ALEX-obiz>
2022-03-30 14:15:28 +0800

    完成

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Assembly;
+using obiz_load_data; 
 
 namespace obiz_SqlCRUD
 {
@@ -15,6 +16,10 @@ namespace obiz_SqlCRUD
     {
         DataTable Dt;
         SchoolEntities db = new SchoolEntities();
+
+        string AppName = "obiz_SqlCRUD_Form1";
+        Msg_log msg_Log = new Msg_log();
+
         public Form1()
         {
             InitializeComponent();
@@ -24,22 +29,34 @@ namespace obiz_SqlCRUD
         //初次設定
         public void setup()
         {
-            String[] items = new String[] {"Name", "StuId","Phone","Address" };
-
-            for (int i = 0; i < items.Length; i++)
+            try
             {
-                Cb_Type.Items.Add(items[i]);
-            }
-            Cb_Type.SelectedIndex = 0;
+                String[] items = new String[] { "Name", "StuId", "Phone", "Address" };
 
-            Dgv_load();
+                for (int i = 0; i < items.Length; i++)
+                {
+                    Cb_Type.Items.Add(items[i]);
+                }
+                Cb_Type.SelectedIndex = 0;
+
+                Dgv_load();
+            }catch(Exception ex)
+            {
+                msg_Log.save_log(AppName, ex);
+            }
         }
 
         //按下新增後
         private void button1_Click(object sender, EventArgs e)
         {
-            Form2 form = new Form2(dataGridView1);
-            form.Show();
+            try
+            {
+                Form2 form = new Form2(dataGridView1);
+                form.Show();
+            }catch(Exception ex)
+            {
+                msg_Log.save_log(AppName, ex);
+            }
         }
 
         //搜尋
@@ -47,17 +64,23 @@ namespace obiz_SqlCRUD
         {
             if(e.KeyCode == Keys.Enter)
             {
-                if (Cb_Type.Text.Length != 0)
+                try
                 {
-                    Dt = null;
-                    string SearchQuery = $"select * from StudentInfo where {Cb_Type.Text} like '%{Tb_Search.Text}%'";
-                    Dt = SqlCRUD.SqlTable(SearchQuery);
+                    if (Cb_Type.Text.Length != 0)
+                    {
+                        Dt = null;
+                        string SearchQuery = $"select * from StudentInfo where {Cb_Type.Text} like '%{Tb_Search.Text}%'";
+                        Dt = SqlCRUD.SqlTable(SearchQuery);
 
-                    dataGridView1.DataSource = Dt;
-                }
-                else
+                        dataGridView1.DataSource = Dt;
+                    }
+                    else
+                    {
+                        Dgv_load();
+                    }
+                }catch(Exception ex)
                 {
-                    Dgv_load();
+                    msg_Log.save_log(AppName, ex);
                 }
             }
         }
@@ -65,40 +88,58 @@ namespace obiz_SqlCRUD
         //重新整理
         public void Dgv_load()
         {
-            string SearchQuery = "select * from StudentInfo";
-            Dt = SqlCRUD.SqlTable(SearchQuery);
+            try
+            {
+                string SearchQuery = "select * from StudentInfo";
+                Dt = SqlCRUD.SqlTable(SearchQuery);
 
-            dataGridView1.DataSource = Dt;
+                dataGridView1.DataSource = Dt;
+            }catch(Exception ex)
+            {
+                msg_Log.save_log(AppName, ex);
+            }
         }
 
         //刪除事件
         private void Btn_Del_Click(object sender, EventArgs e)
         {
-            if(Tb_num.Text.Length != 0)
+            try
             {
-                string Select_Query = $"select * from StudentInfo where id = {Tb_num.Text}";
-                DataTable dt = SqlCRUD.SqlTable(Select_Query);
-                if(dt.Rows.Count > 0)
+                if (Tb_num.Text.Length != 0)
                 {
-                    string Update_Query = $"Delete StudentInfo where Id = {Tb_num.Text}";
-                    new SqlCRUD(Update_Query);
-                    Dgv_load();
+                    string Select_Query = $"select * from StudentInfo where id = {Tb_num.Text}";
+                    DataTable dt = SqlCRUD.SqlTable(Select_Query);
+                    if (dt.Rows.Count > 0)
+                    {
+                        string Update_Query = $"Delete StudentInfo where Id = {Tb_num.Text}";
+                        new SqlCRUD(Update_Query);
+                        Dgv_load();
+                    }
                 }
+            }catch(Exception ex)
+            {
+                msg_Log.save_log(AppName, ex);
             }
         }
         //編輯
         private void Btn_Edit_Click(object sender, EventArgs e)
         {
-            if (Tb_num.Text.Length != 0)
+            try
             {
-                string Select_Query = $"select * from StudentInfo where id = {Tb_num.Text}";
-                DataTable dt = SqlCRUD.SqlTable(Select_Query);
-                if (dt.Rows.Count > 0)
+                if (Tb_num.Text.Length != 0)
                 {
-                    string Update_Query = $"UPDATE  StudentInfo SET Name={Tb_Name.Text},StuID={Tb_Search.Text},Phone={Tb_Phone.Text},Address={Tb_Address.Text} where Id = {Tb_num.Text}";
-                    new SqlCRUD(Update_Query);
-                    Dgv_load();
+                    string Select_Query = $"select * from StudentInfo where id = {Tb_num.Text}";
+                    DataTable dt = SqlCRUD.SqlTable(Select_Query);
+                    if (dt.Rows.Count > 0)
+                    {
+                        string Update_Query = $"UPDATE  StudentInfo SET Name={Tb_Name.Text},StuID={Tb_Search.Text},Phone={Tb_Phone.Text},Address={Tb_Address.Text} where Id = {Tb_num.Text}";
+                        new SqlCRUD(Update_Query);
+                        Dgv_load();
+                    }
                 }
+            }catch(Exception ex)
+            {
+                msg_Log.save_log(AppName, ex);
             }
         }
     }
